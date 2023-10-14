4299ad42c28f37dc4c634d55f8258f6a93898e6b
yh9035926 <109195043+yh9035926@users.noreply.github.com>
2023-10-14 11:11:40 +0900

    e.key==="Enter"

--- src/pages/WordGame.js
+++ src/pages/WordGame.js
@@ -1,11 +1,81 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+import useInput from "../hooks/useInput";
 
 const WordGame = () => {
-  return (
-    <div>
-      쿵쿵따
-    </div>
-  );
+  const [wordInput, onChangeWordInput, setWordInput] = useInput();
+  const [numberInput, onChangeNumberInput, setNumberInput] = useInput();
+  const [word, setWord] = useState("");
+  const [start, setStart] = useState(false);
+  const [number, setNumber] = useState("");
+  const [order, setOrder] = useState(1);
+  const wordRef = useRef(null);
+
+  function onNumberBtn() {
+    if (!isNaN(numberInput)) {
+      // isNaN 숫자가 있으면 false
+      setNumber(numberInput);
+      setStart(true);
+    }
+  }
+
+  function onWordBtn() {
+    if (number) {
+      if (
+        (!word && wordInput.length === 3) ||
+        (word[word.length - 1] === wordInput[0] && wordInput.length === 3)
+      ) {
+        setWord(wordInput);
+        if (order + 1 > number) {
+          setOrder(1);
+        } else {
+          setOrder(order + 1);
+        }
+      } else {
+        alert("세 글자만 됩니다");
+      }
+      setWordInput("");
+      wordRef.current.focus();
+    }
+  }
+
+  function onEnter(e) {
+    if (e.key === "Enter") {
+      onWordBtn();
+    }
+  }
+
+  if (start) {
+    return (
+      <div>
+        <div>
+          <span>{number}명 참가</span>
+        </div>
+        <div>{order}번째 차례</div>
+        <div>
+          제시어: <span id="word">{word}</span>
+        </div>
+        <input
+          ref={wordRef}
+          type="text"
+          value={wordInput}
+          onChange={onChangeWordInput}
+          onKeyDown={onEnter}
+        />
+        <button onClick={onWordBtn}>입력</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <input
+          placeholder="참가자 몇 명?"
+          value={numberInput}
+          onChange={onChangeNumberInput}
+        />
+        <button onClick={onNumberBtn}>확인</button>
+      </div>
+    );
+  }
 };
 
 export default WordGame;
