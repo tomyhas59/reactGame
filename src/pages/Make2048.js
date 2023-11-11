e152bddc9748ce7dd9adaac2f9089dd757362d9e
yh9035926 <yh9035926@naver.com>
2023-11-12 01:33:28 +0900

    make2048

--- src/pages/Make2048.js
+++ src/pages/Make2048.js
@@ -4,6 +4,7 @@ const Game2048 = () => {
   const [data, setData] = useState([]);
   const [score, setScore] = useState(0);
   const [history, setHistory] = useState([]);
+  const [reGame, setReGame] = useState(false);
 
   const startGame = useCallback(() => {
     const initialData = Array.from({ length: 4 }, () =>
@@ -12,6 +13,7 @@ const Game2048 = () => {
     const newData = put2ToRandomCell(initialData);
     setData(newData);
     setScore(0);
+    setReGame(false);
   }, []);
 
   useEffect(() => {
@@ -36,7 +38,145 @@ const Game2048 = () => {
     return newData;
   };
 
-  const moveCells = useCallback([data, score]);
+  const reGameStart = useCallback(() => {
+    startGame();
+  }, [startGame]);
+
+  const back = useCallback(() => {
+    const updatedHistory = [...history]; // 변경된 히스토리를 복사
+    const prevData = updatedHistory.pop(); // 마지막 요소 가져오기
+    if (!prevData) return;
+    setScore(prevData.score);
+    setData(prevData.table);
+    setHistory(updatedHistory); // 업데이트된 히스토리 설정
+  }, [history, setData, setScore, setHistory]);
+
+  const moveCells = useCallback(
+    (direction) => {
+      setHistory((prevHistory) => [
+        ...prevHistory,
+        {
+          table: JSON.parse(JSON.stringify(data)),
+          score: score,
+        },
+      ]);
+      switch (direction) {
+        case "left": {
+          const newData = [[], [], [], []]; //배열 초기화 (이동 후 데이터 담을 공간)
+          data.forEach((rowData, i) => {
+            rowData.forEach((cellData, j) => {
+              if (cellData) {
+                const currentRow = newData[i]; //현재 행의 새로운 데이터 배열
+                const prevData = currentRow[currentRow.length - 1];
+                if (prevData === cellData) {
+                  setScore(score + currentRow[currentRow.length - 1] * 2);
+                  currentRow[currentRow.length - 1] *= -2;
+                } else {
+                  newData[i].push(cellData);
+                }
+              }
+            });
+          });
+          console.log(newData);
+          [1, 2, 3, 4].forEach((rowData, i) => {
+            [1, 2, 3, 4].forEach((cellData, j) => {
+              data[i][j] = Math.abs(newData[i][j]) || 0;
+            });
+          });
+
+          break;
+        }
+        case "right": {
+          const newData = [[], [], [], []];
+          data.forEach((rowData, i) => {
+            rowData.forEach((cellData, j) => {
+              if (rowData[3 - j]) {
+                const currentRow = newData[i];
+                const prevData = currentRow[currentRow.length - 1];
+                if (prevData === rowData[3 - j]) {
+                  setScore(score + currentRow[currentRow.length - 1] * 2);
+                  currentRow[currentRow.length - 1] *= -2;
+                } else {
+                  newData[i].push(rowData[3 - j]);
+                }
+              }
+            });
+          });
+          console.log(newData);
+          [1, 2, 3, 4].forEach((rowData, i) => {
+            [1, 2, 3, 4].forEach((cellData, j) => {
+              data[i][3 - j] = Math.abs(newData[i][j]) || 0;
+            });
+          });
+
+          break;
+        }
+        case "up": {
+          const newData = [[], [], [], []];
+          data.forEach((rowData, i) => {
+            rowData.forEach((cellData, j) => {
+              if (cellData) {
+                const currentRow = newData[j];
+                const prevData = currentRow[currentRow.length - 1];
+                if (prevData === cellData) {
+                  setScore(score + currentRow[currentRow.length - 1] * 2);
+                  currentRow[currentRow.length - 1] *= -2;
+                } else {
+                  newData[j].push(cellData);
+                }
+              }
+            });
+          });
+          console.log(newData);
+          [1, 2, 3, 4].forEach((cellData, i) => {
+            [1, 2, 3, 4].forEach((rowData, j) => {
+              data[j][i] = Math.abs(newData[i][j]) || 0;
+            });
+          });
+
+          break;
+        }
+        case "down": {
+          const newData = [[], [], [], []];
+          data.forEach((rowData, i) => {
+            rowData.forEach((cellData, j) => {
+              if (data[3 - i][j]) {
+                const currentRow = newData[j];
+                const prevData = currentRow[currentRow.length - 1];
+                if (prevData === data[3 - i][j]) {
+                  setScore(score + currentRow[currentRow.length - 1] * 2);
+                  currentRow[currentRow.length - 1] *= -2;
+                } else {
+                  newData[j].push(data[3 - i][j]);
+                }
+              }
+            });
+          });
+          console.log(newData);
+          [1, 2, 3, 4].forEach((cellData, i) => {
+            [1, 2, 3, 4].forEach((rowData, j) => {
+              data[3 - j][i] = Math.abs(newData[i][j]) || 0;
+            });
+          });
+          break;
+        }
+        default:
+          break;
+      }
+      if (data.flat().includes(2048)) {
+        setTimeout(() => {
+          alert("승리!");
+          setReGame(true);
+        }, 500);
+      } else if (!data.flat().includes(0)) {
+        alert("패배");
+        setReGame(true);
+      } else {
+        setData(put2ToRandomCell(data));
+      }
+    },
+    [data, score, setHistory]
+  );
 
   useEffect(() => {
     const handleKeyPress = (e) => {
@@ -73,14 +213,10 @@ const Game2048 = () => {
       </table>
 
       <div id="score">{score}</div>
-      <button
-        id="back"
-        onClick={() => {
-          /* 히스토리로 되돌아가는 로직 */
-        }}
-      >
+      <button id="back" onClick={back}>
         뒤로
       </button>
+      {reGame && <button onClick={reGameStart}>다시 하기</button>}
     </div>
   );
 };
